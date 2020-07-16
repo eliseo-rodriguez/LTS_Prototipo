70484889d0337f7efa519af4c17e33b833aedb6e
Eliseo <eliseo_rodriguez@latisinformatica.com>
2020-07-15 20:18:49 -0400

    PrdBrr, Msg Refacto

---
+++ LTS_Prototipo.Web/Util/Util.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LTS_Proto.Web {
+    public static class Util {
+        public static (string, string) Msg_Set(string msg = "", string msgCls = "alert-info") {
+            return (msg, msgCls);
+        }
+    }
+}
